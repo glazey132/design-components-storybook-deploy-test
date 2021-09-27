0d4df3f1526849e28aeb9d30b3d355613676628d
Alex Glaze <aglaze@joinhandshake.com>
2021-09-27 03:09:30 -0500

    use affected

--- apps/handshake-design-components/src/testButton/testButton.tsx
+++ apps/handshake-design-components/src/testButton/testButton.tsx
@@ -20,7 +20,7 @@ export function TestButton() {
         <img src={star} className="material-icons" alt="" />
         Star
         
-          <button className="test-button">TestButton woooo!!!!!</button>
+          <button className="test-button">TestButton woooo!!!!! This should be super quick</button>
       </div>
     </StyledTestButton>
   );
